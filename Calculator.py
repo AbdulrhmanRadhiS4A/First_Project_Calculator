42fab3685bd7dadfedda87cc2dbe46b898b95ceb
AbdulrhmanRadhiS4A <60300843+AbdulrhmanRadhiS4A@users.noreply.github.com>
2023-03-14 00:43:04 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,23 +1,27 @@
-from Operations  import opeartion as oper
+from Operations import opeartion as oper
+
 #  User Select  the operation
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
-print("3.Multiply") 
+print("3.Multiply")
 print("4.Divide")
+print("5.Mod")
+
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '5'):
         try:
-            num1 = float(input("Enter first number: "))# user enter first number (operand)
-            num2 = float(input("Enter second number: "))# user enter second number (operand)
+            num1 = float(input("Enter first number: "))  # user enter first number (operand)
+            num2 = float(input("Enter second number: "))  # user enter second number (operand)1
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
+
         print("Result of Operation = ")
         if choice == '1':
             print(num1, "+", num2, "=", oper.add(num1, num2))
@@ -29,12 +33,24 @@ while True:
             print(num1, "*", num2, "=", oper.multiply(num1, num2))
 
         elif choice == '4':
-            print(num1, "/", num2, "=",oper.divide(num1, num2))
-        
+            try:
+                print(num1, "/", num2, "=", oper.divide(num1, num2))
+
+            except ZeroDivisionError:
+                print("Can not divided by Zero")
+        elif choice == '5':
+            print(num1, "%", num2, "=", oper.mod(num1, num2))
+
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
+
+        while next_calculation not in ('yes', 'y'):
+            if next_calculation in ('no', 'n'):
+                print("Exiting ...")
+                exit()
+            print("to continue type 'yes' or 'y' !")
+            next_calculation = input("Let's do next calculation? (yes/no): ")
+
     else:
         print("Invalid Input")
